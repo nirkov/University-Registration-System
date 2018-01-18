5a65414af296c6735fdbe1a3b02aa4d66e63ac94
nirkov <32679759+nirkov@users.noreply.github.com>
2018-01-19 00:36:13 +0200

    Update ActorThreadPool.java

--- ActorsThreadPool/src/main/java/bgu/spl/a2/ActorThreadPool.java
+++ ActorsThreadPool/src/main/java/bgu/spl/a2/ActorThreadPool.java
@@ -21,13 +21,13 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * methods
  */
 public class ActorThreadPool {
-	private ConcurrentHashMap<String , ConcurrentLinkedQueue<Action>>   mapOfActionQ;			//each cell is an actor implemented as Queue
-	private ConcurrentHashMap<String , PrivateState>                    mapOfPS;				//each cell is an private state
-	private Queue<String> 											    actorWaitingToExecute;	//list of actor's name of not empty actors
-	private ArrayList<Thread> 									        threadPool;				//list of Threda
-	private ConcurrentHashMap<String , Boolean>				            mapLockedActor;			//each cell is a boolean flag is actor locked: true=locked
-	private VersionMonitor 											    VM;
-	private AtomicBoolean 												SHUTDOWN;
+	private ConcurrentHashMap<String , ConcurrentLinkedQueue<Action>>   mapOfActionQ;          //each cell is an actor implemented as Queue
+	private ConcurrentHashMap<String , PrivateState>                    mapOfPS;		   //each cell is an private state
+	private Queue<String> 					            actorWaitingToExecute; //list of actor's name of not empty actors
+	private ArrayList<Thread> 		                            threadPool;		   //list of Threda
+	private ConcurrentHashMap<String , Boolean>			    mapLockedActor;        //each cell is a boolean flag is actor locked: true=locked
+	private VersionMonitor 						    VM;
+	private AtomicBoolean 						    SHUTDOWN;
 
 	/**
 	 * creates a {@link ActorThreadPool} which has nthreads. Note, threads
@@ -43,10 +43,10 @@ public class ActorThreadPool {
 	 */
 	public ActorThreadPool(int nthreads) {
 		SHUTDOWN = new AtomicBoolean(false);
-		mapOfActionQ = new ConcurrentHashMap<>();					//create map of action queue which each queue represent Actor
-		mapOfPS = new ConcurrentHashMap<>() ;						//create map of private state of actor
-		actorWaitingToExecute = new LinkedList<>();					//list of unlocked Actors
-		mapLockedActor = new  ConcurrentHashMap<>();				//Map for idicate which Actor is locked
+		mapOfActionQ = new ConcurrentHashMap<>();		//create map of action queue which each queue represent Actor
+		mapOfPS = new ConcurrentHashMap<>() ;			//create map of private state of actor
+		actorWaitingToExecute = new LinkedList<>();		//list of unlocked Actors
+		mapLockedActor = new  ConcurrentHashMap<>();		//Map for idicate which Actor is locked
 		createThreadPool(nthreads);
 		VM = new VersionMonitor();
 	}
@@ -64,12 +64,12 @@ public class ActorThreadPool {
 	 */
 	public synchronized void submit(Action<?> action, String actorId, PrivateState actorState) {
 		ConcurrentLinkedQueue<Action> newActorQ = mapOfActionQ.get(actorId);
-		if(newActorQ == null) {					 						//if this action not exists in the table we create a new action's queue -
-			newActorQ = new ConcurrentLinkedQueue<>();					//create new actor implement as queue
-			newActorQ.add(action);										//add the recived action to the actor that we created
-			mapOfActionQ.put(actorId, newActorQ);						//mapped the new Actor to hashmap of actors - mapped by actorId
-			setPrivateState(actorId, actorState);						//mapped the private state of this actor - mapped by actorId
-			actorWaitingToExecute.add(actorId);							//we must add this actor to waiting list in this case because this is a new actor
+		if(newActorQ == null) {					//if this action not exists in the table we create a new action's queue -
+			newActorQ = new ConcurrentLinkedQueue<>();	//create new actor implement as queue
+			newActorQ.add(action);				//add the recived action to the actor that we created
+			mapOfActionQ.put(actorId, newActorQ);		//mapped the new Actor to hashmap of actors - mapped by actorId
+			setPrivateState(actorId, actorState);		//mapped the private state of this actor - mapped by actorId
+			actorWaitingToExecute.add(actorId);		//we must add this actor to waiting list in this case because this is a new actor
 			mapLockedActor.put(actorId , false);
 		}else{
 			newActorQ.add(action);
@@ -109,7 +109,7 @@ public class ActorThreadPool {
 
 
 //******************************************************************************************************
-//												FUNCTIONS
+//						FUNCTIONS
 //******************************************************************************************************
 
 	private void addToWaitingList(Queue<String> addto ,String actorId ) {
@@ -121,24 +121,24 @@ public class ActorThreadPool {
 	private void createThreadPool(int nthreads){
 		threadPool = new ArrayList<>();
 		for(int i = 0 ; i < nthreads ; i++) {
-			Runnable task = () -> {													//Runnable task as a Lambda function for each thread to execute
-				while(!SHUTDOWN.get()) {
-					String nextActorID = nextUnlockedActor();							//the next actor with action to execute
-					if(nextActorID != null) {
-						Action toExecute = mapOfActionQ.get(nextActorID).poll();		//take action to execute
-						toExecute.handle(this, nextActorID, getPrivaetState(nextActorID));
-						endOfMission( nextActorID);
-					}else {
-						if(!SHUTDOWN.get()){
-							try {
-								VM.await(VM.getVersion()+1);
-							} catch (InterruptedException e) {
-								e.printStackTrace();
-							}
-						}
-					}
+		    Runnable task = () -> {													//Runnable task as a Lambda function for each thread to execute
+			while(!SHUTDOWN.get()) {
+			    String nextActorID = nextUnlockedActor();		//the next actor with action to execute
+			    if(nextActorID != null) {
+				Action toExecute = mapOfActionQ.get(nextActorID).poll();       //take action to execute
+				toExecute.handle(this, nextActorID, getPrivaetState(nextActorID));
+				endOfMission( nextActorID);
+			    }else {
+				if(!SHUTDOWN.get()){
+				    try {
+					VM.await(VM.getVersion()+1);
+				    } catch (InterruptedException e) {
+					e.printStackTrace();
+				    }
 				}
-				VM.inc();
+			    }
+			}
+			VM.inc();
 			};
 			threadPool.add(new Thread(task));
 		}
@@ -151,27 +151,27 @@ public class ActorThreadPool {
 	// in, finds a free ACTOR and locks it from other threads
 
 	private synchronized String nextUnlockedActor() {
-		String nextUnlocked = null;
-		if(!actorWaitingToExecute.isEmpty()) {
-			int size = actorWaitingToExecute.size();
-			boolean found = false;
-			while(size != 0 && !found) {
-				nextUnlocked = actorWaitingToExecute.peek();											//take peek of waiting actors
-				if(!mapLockedActor.get(nextUnlocked) && !mapOfActionQ.get(nextUnlocked).isEmpty()){		//if not locked and not empty we return this actor's name
-					found = true;
-					nextUnlocked = actorWaitingToExecute.peek();
-					mapLockedActor.put(nextUnlocked , true);			    //this thread lock this actor from other threads
-				}else {														//else we want to delete this actor if he empty or insert him in the end of the queue if he locked
-					size--;
-					nextUnlocked =  actorWaitingToExecute.poll();
-					if(!mapOfActionQ.get(nextUnlocked).isEmpty()) {
-						actorWaitingToExecute.add(nextUnlocked);
-					}
-					nextUnlocked = null;
-				}
+	    String nextUnlocked = null;
+	    if(!actorWaitingToExecute.isEmpty()) {
+		int size = actorWaitingToExecute.size();
+		boolean found = false;
+		while(size != 0 && !found) {
+	            nextUnlocked = actorWaitingToExecute.peek();											//take peek of waiting actors
+    		    if(!mapLockedActor.get(nextUnlocked) && !mapOfActionQ.get(nextUnlocked).isEmpty()){		//if not locked and not empty we return this actor's name
+			found = true;
+			nextUnlocked = actorWaitingToExecute.peek();
+			mapLockedActor.put(nextUnlocked , true);	 //this thread lock this actor from other threads
+		    }else {														//else we want to delete this actor if he empty or insert him in the end of the queue if he locked
+			size--;
+			nextUnlocked =  actorWaitingToExecute.poll();
+			if(!mapOfActionQ.get(nextUnlocked).isEmpty()) {
+		   	    actorWaitingToExecute.add(nextUnlocked);
 			}
+			nextUnlocked = null;
+		    }
 		}
-		return nextUnlocked;
+	    }
+	    return nextUnlocked;
 	}
 
 	private void endOfMission(String nextActorID) {	// should not be syncronize because only one thread work on this Actor
