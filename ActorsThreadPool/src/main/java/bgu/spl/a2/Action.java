46de06822fed6fcdb273cfe66676ae6ec2567cb4
nirkov <32679759+nirkov@users.noreply.github.com>
2018-01-27 01:13:08 +0200

    Update Action.java

--- ActorsThreadPool/src/main/java/bgu/spl/a2/Action.java
+++ ActorsThreadPool/src/main/java/bgu/spl/a2/Action.java
@@ -8,12 +8,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * an abstract class that represents an action that may be executed using the
  * {@link ActorThreadPool}
- *
- * Note for implementors: you may add methods and synchronize any of the
- * existing methods in this class BUT you must be able to explain why the
- * synchronization is needed. In addition, the methods you add to this class can
- * only be private!!!
- *
  * @param <R> the action result type
  */
 public abstract class Action<R> {
@@ -25,25 +19,13 @@ public abstract class Action<R> {
     private AtomicInteger numOfActionThisDepend;
     private String actorId;
     protected PrivateState myActorPS;
-    /**
-     * start handling the action - note that this method is protected, a thread
-     * cannot call it directly.
-     */
+ 
+    
     protected abstract void start();
 
 
-
     /**
-     *
      * start/continue handling the action
-     *
-     * this method should be called in order to start this action
-     * or continue its execution in the case where it has been already started.
-     *
-     * IMPORTANT: this method is package protected, i.e., only classes inside
-     * the same package can access it - you should not change it to
-     * public/private/protected
-     *
      */
     /*package*/
     final void handle(ActorThreadPool pool, String actorId, PrivateState actorState) {
@@ -61,10 +43,6 @@ public abstract class Action<R> {
     /**
      * add a callback to be executed once all the given actions results are
      * resolved
-     *
-     * Implementors note: make sure that the callback is running only once when
-     * all the given actions completed.
-     *
      * @param actions
      * @param callback the callback to execute once all the results are resolved
      */
@@ -79,7 +57,6 @@ public abstract class Action<R> {
                     sendMessage(this , actorId , myActorPS);
                 }
             });
-
         }
     }
 
