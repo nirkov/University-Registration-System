e351d38d46c91690346a8bd6f8cab86b62b70f9e
Nir Kovrovski <32679759+nirkov@users.noreply.github.com>
2018-08-01 13:28:23 +0300

    Update StudentPrivateState.java

--- ActorsThreadPool/src/main/java/bgu/spl/a2/sim/privateStates/StudentPrivateState.java
+++ ActorsThreadPool/src/main/java/bgu/spl/a2/sim/privateStates/StudentPrivateState.java
@@ -12,11 +12,6 @@ public class StudentPrivateState extends PrivateState{
 	private HashMap<String, Integer> grades;
 	private long signature;
 
-	/**
-	 * Implementors note: you may not add other constructors to this class nor
-	 * you allowed to add any other parameter to this constructor - changing
-	 * this may cause automatic tests to fail..
-	 */
 	public StudentPrivateState() {
 		grades = new HashMap<String, Integer>();
 		signature = 0;
